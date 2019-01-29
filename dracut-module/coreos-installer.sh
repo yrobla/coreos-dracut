9b5fc4132295128da7f6c6dee8456a0b47917af2
Yolanda Robla <yroblamo@redhat.com>
2019-01-29 16:03:26 +0100

    Add fixes on ign write

--- dracut-module/coreos-installer.sh
+++ dracut-module/coreos-installer.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2019 Red Hat, Inc.
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 udevadm trigger
 udevadm settle
 
@@ -81,7 +95,9 @@ DEST_DEV=/dev/$DEST_DEV
 # Query for the ignition endpoint
 #########################################################
 echo "Querying for ignition endpoint" >> /tmp/debug
-get_ignition_file
+if [ "$IGNITION_URL" != "skip" ];then
+    get_ignition_file
+fi
 
 #########################################################
 #Create the tmpfs filesystem to store the image
@@ -117,9 +133,8 @@ udevadm settle
 #########################################################
 # If one was provided, install the ignition config
 #########################################################
-echo "ignition is $IGNITION_URL"
-if [ "$IGNITION_URL" != "skip" ];then
-    write_ignition
+if [ "$FINAL_IGNITION" != "" ];then
+    write_ignition_file
 fi
 
 if [ ! -f /tmp/skip_reboot ]
